1abbacc5ddd430dcfbf2b01e02cb606e0076c166
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:22:50 -0400

    Add Test Project/Test ProjectTests/Test_ProjectTests.swift

---
+++ Test Project/Test ProjectTests/Test_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Test_ProjectTests.swift
+//  Test ProjectTests
+//
+//  Created by Elliot Williams on 2025-06-06.
+//
+
+import Testing
+@testable import Test_Project
+
+struct Test_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
