82b492cc637d436735df08a3d9588724c595adba
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:22:24 -0400

    Add Test Project/Test Project/Test_ProjectApp.swift

---
+++ Test Project/Test Project/Test_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_ProjectApp.swift
+//  Test Project
+//
+//  Created by Elliot Williams on 2025-06-06.
+//
+
+import SwiftUI
+
+@main
+struct Test_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CoverflowCarousel()
+        }
+    }
+}
